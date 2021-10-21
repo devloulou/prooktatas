b5afd02cd38a3bf6187552657b8e70c7c8482d62
kovacs.richard90@gmail.com <dev.kovacs.richard90@gmail.com>
2021-10-21 21:15:47 +0200

    change comment

--- 3.alkalom/is_vs_egyelo.py
+++ 3.alkalom/is_vs_egyelo.py
@@ -1,38 +1,38 @@
-# list1 = []
-# list2 = []
-# list3=list1
+list1 = []
+list2 = []
+list3=list1
 
-# # print(id(list1))
-# # print(id(list2))
-# # print(id(list3))
-# # exit()
+# print(id(list1))
+# print(id(list2))
+# print(id(list3))
+# exit()
  
-# if (list1 == list2):
-#     print("True")
-# else:
-#     print("False")
+if (list1 == list2):
+    print("True")
+else:
+    print("False")
  
-# if (list1 is list2):
-#     print("True")
-# else:
-#     print("False")
+if (list1 is list2):
+    print("True")
+else:
+    print("False")
  
-# if (list1 is list3):
-#     print("True")
-# else:   
-#     print("False")
+if (list1 is list3):
+    print("True")
+else:   
+    print("False")
  
-# list3 = list3 + [2]
+list3 = list3 + [2]
 
-# list3.append(1)
+list3.append(1)
 
-# print(f"list1: {list1}")
-# print(f"list1: {list3}")
+print(f"list1: {list1}")
+print(f"list1: {list3}")
  
-# if (list1 is list3):
-#     print("True")
-# else:   
-#     print("False")
+if (list1 is list3):
+    print("True")
+else:   
+    print("False")
 
 # #and or not
 a = 0
@@ -40,31 +40,31 @@ b = 0
 
 
 
-# if (a > 1) or (b > 1) or (b == 3) or (1 == 0):
-#     print(a+b)
-# else:
-#     print("else ág")
+if (a > 1) or (b > 1) or (b == 3) or (1 == 0):
+    print(a+b)
+else:
+    print("else ág")
 
-# if (a > 1) and (b > 1) and (b == 3) and (1 == 1):
-#     print(a+b)
-# else:
-#     print("else ág")
+if (a > 1) and (b > 1) and (b == 3) and (1 == 1):
+    print(a+b)
+else:
+    print("else ág")
 
-# print(1==0)
+print(1==0)
 
-# if (a == 0) and (b == 0) and (b == a) and (1 == 1):
-#     print(a == 0)
-#     print(b == 0)
-#     print(a+b)
-# else:
-#     print("else ág")
+if (a == 0) and (b == 0) and (b == a) and (1 == 1):
+    print(a == 0)
+    print(b == 0)
+    print(a+b)
+else:
+    print("else ág")
 
-# if ((a == 0) and (b == 0)) or ((b == a) and (1 == 1)):
-#     print(a == 0)
-#     print(b == 0)
-#     print(a+b)
-# else:
-#     print("else ág")
+if ((a == 0) and (b == 0)) or ((b == a) and (1 == 1)):
+    print(a == 0)
+    print(b == 0)
+    print(a+b)
+else:
+    print("else ág")
 
 
 a = False
