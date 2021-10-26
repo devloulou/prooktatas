5f4ebaa1bdc69e6083057b7f0cd9f23b80473e02
kovacs.richard90@gmail.com <dev.kovacs.richard90@gmail.com>
2021-10-26 18:05:32 +0200

    feladatok megoldása

--- 4. alkalom/teszt_feladatok.py
+++ 4. alkalom/teszt_feladatok.py
@@ -1,17 +1,25 @@
 # 1. feladat:
 # a my_list2 listába helyezzük át a az utolsó 4 elemet a my_list listából
 # Törekedjetek az tanult megoldások használatára
+
 my_list = ["alma", "Géza", int, 1, 2, 3, 4, 5]
 my_list2 = []
+
+my_list2 = my_list[-4:]
+
+#print(my_list2)
 
 
 # 2. feladat: a my_list litában lévő duplikációkat szüntessük meg.
 # Törekedjetek a tanult megoldások használatára
 my_list = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6]
 
+my_list = list(set(my_list))
+# print(list(set(my_list)))
+# print(my_list)
+
 # 3. feladat: Adjatok egy új állás-t az állások-hoz. 
 # Kérlek, hogy ne ciklussal oldjátok meg a feladatot, hanem dict műveletekkel, manuálisan
-
 
 # A feladat megoldása során törekedjetek a dictionary / egyéb műveletek használatára.
 # Maga a dict struktúrája ne változzon.
@@ -42,13 +50,45 @@ my_dict = {
 
 }
 
+# my_dict['állások'].update({"saját": {
+#                                 "pozíció": "Fejlesztő",
+#                                 "fizetés": "600000",
+#                                 "cég": "Ricsi"
+#                             }})
+
+my_dict['állások']['saját'] ={
+                                "pozíció": "Fejlesztő",
+                                "fizetés": "600000",
+                                "cég": "Ricsi"
+                            }
+
+
+#print(my_dict)
+
 # 3.1 feladat: Töröljétek az Egyéb hirdetést. A többi állás változatlan maradjon.
+
+my_dict['állások'].pop("Egyéb")
+
+#print(my_dict)
+
 # 3.2 feladat: Ahol a fizetés string, ott a fizetés legyen szám típusú. Kérlek, hogy ne ciklussal
 # oldjátok meg, hanem manuálisan. Vezérlő szerkezetet használhattok (de nem szükséges).
+
+my_dict['állások']["Profession.hu"]["fizetés"] = int(my_dict['állások']["Profession.hu"]["fizetés"])
+
+# print(my_dict)
+# print()
 
 # 4. feladat: a my_list-ben megadott elemeket bontsuk ki, és egy listában legyenek az elemek
 # pl: lista[1,2,3,4,5,6,7,8,9]
 my_list = [[1,2,3],[4,5,6],[7,8,9]]
+
+#my_list = [*my_list[0], *my_list[1], *my_list[2]]
+
+my_list2 = my_list[0] + my_list[1] + my_list[2]
+# print(my_list)
+# print(my_list2)
+
 
 # 5. feladat: a my_tuple változóban lévő adatokban vannak Error-al kezdődő értékek.
 # Az eddig tanultak  használatával oldjuk meg, hogy az Error-al kezdődő értékek ne szerepeljenek a tuple-ben.
@@ -57,3 +97,7 @@ my_list = [[1,2,3],[4,5,6],[7,8,9]]
 # A végén a my_tuple = ("12", "14") eredményt szeretném látni.
 
 my_tuple = ("Error in line 99", "12", "14", "Error in line 45")
+
+my_tuple2 = tuple(my_tuple[1:-1])
+
+print(my_tuple2)
