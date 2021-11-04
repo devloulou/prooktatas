7e28ea5e352c94bc68bee1a9000e861a8503a161
kovacs.richard90@gmail.com <dev.kovacs.richard90@gmail.com>
2021-11-04 18:55:29 +0100

    feladatok

--- 6. alkalom/task.py
+++ 6. alkalom/task.py
@@ -1,2 +1,6 @@
 from my_utils.file_wrapper import read_txt
 
+# ezt folytatjuk - példa arra, hogy miért jó a package és modul
+# feladat: a dinamt.txt file-ből töröljük azokat a sorokat, amelyek tartalmazzák
+# valamelyiket a következők közül: Gordon, Feri, Peti, Gyula
+# szükség esetén egészítsük ki a package-et a megfelelő funkcionalitással
--- 7. alkalom/feladatok.py
+++ 7. alkalom/feladatok.py
@@ -3,7 +3,8 @@
 # 1.2 feladat: Listázzuk az összes kulcs értéket a megadott dictionaryből
 # 1.3 feladat: Listázzuk az összes olyan auto-t, amelynek a motorja 1.4-esnél nagyobb.
 
-my_dict = {'prev_year':{
+my_dict = {
+    'prev_year': {
         'auto': [
     {
         'color': "Purple",
@@ -49,7 +50,60 @@ my_dict = {'prev_year':{
     }
 }
 
+#Listázzuk ki a megadott dictionary-ból az autók típusait.
+#pl: my_list = ['Mercedes', 'Volvo', 'BMW']
+
+my_list = []
+
+print(my_dict['prev_year']['auto'][0]['motor'])
+print(my_dict['act_year']['auto'][0]['motor'])
+print(type(my_dict['prev_year']['auto'][0]['motor']))
+
+##### 
+## 'prev_year' -> key
+## my_dict['prev_year'] -> value
+
+for key, value in my_dict.items():
+    for item in value['auto']:
+        if item['motor'] > 1.4:
+            my_list.append(item['type'])
+
+print(my_list)
+exit()
+
+for key, value in my_dict.items():
+    for item in value['auto']:
+        my_list.append(item['type'])
+
+#print(my_list)
+
+my_list = []
+my_list2 = []
+
+for key, value in my_dict.items():
+    my_list.append(key)
+    for k, v in value.items():
+        my_list.append(k)
+        for item in v:
+            my_list.extend(item.keys())
 
 
+my_list2.extend(my_dict.keys())
+my_list2.extend(my_dict['prev_year'].keys())
+my_list2.extend(my_dict['act_year'].keys())
+my_list2.extend(my_dict['prev_year']['auto'][0].keys())
+my_list2.extend(my_dict['prev_year']['auto'][1].keys())
+my_list2.extend(my_dict['prev_year']['auto'][2].keys())
+my_list2.extend(my_dict['act_year']['auto'][0].keys())
+my_list2.extend(my_dict['act_year']['auto'][1].keys())
+my_list2.extend(my_dict['act_year']['auto'][2].keys())
 
+print(my_list)
+print(my_list2)
+
+my_list = []
+
+for key, value in my_dict.items():
+    for item in value['auto']:
+        my_list.append(item['type'])
 
