190f966b942d2c00a4e76d5ce63637b260c80a83
kovacs.richard90@gmail.com <dev.kovacs.richard90@gmail.com>
2021-11-04 18:55:44 +0100

    json struktúra

---
+++ 7. alkalom/json_bevezetes.py
@@ -0,0 +1,21 @@
+
+# JSON string
+json_pelda = """{
+
+"key": "value"
+
+}"""
+
+#JSON object: {"auto": "mercedes", "color": "blue"}
+json_pelda = """{
+"key": {"auto": "mercedes", "color": "blue"}
+}"""
+
+# JSON Array
+json_pelda = """{
+"key": [
+    {"firstName":"John", "lastName":"Doe"},
+    {"firstName":"Anna", "lastName":"Smith"},
+    {"firstName":"Peter", "lastName":"Jones"}
+    ]
+}"""
